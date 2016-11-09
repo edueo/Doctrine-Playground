d01f110a94158d89986e9e7830262fb41e9fa9f0
Eduardo Alencar <ealencar10@gmail.com>
2016-11-08 23:49:05 -0200

    Configuring doctrine command-line tools

--- blog/src/bootstrap.php
+++ blog/src/bootstrap.php
@@ -3,6 +3,10 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/config.php';
 
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+
 // Doctrine Entity Manager
 $entitiesPath = array(__DIR__.'/Blog/Entity');
 $config = Setup::createAnnotationMetadataConfiguration($entitiesPath, $dev);
